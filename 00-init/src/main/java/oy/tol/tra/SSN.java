e4913a62a4f1ced7eff73d88987a142380f4f041
vailunka19 <vailunka19@student.oulu.fi>
2022-11-14 16:10:13 +0200

    muutettiin 00init tiedostoa riviltä 103

--- 00-init/src/main/java/oy/tol/tra/SSN.java
+++ 00-init/src/main/java/oy/tol/tra/SSN.java
@@ -100,6 +100,7 @@ public class SSN {
                String personNumberString = ssn.substring(PERSON_CODE_INDEX_START,
                      PERSON_CODE_INDEX_START + PERSON_CODE_LENGTH);
                // Next checking the three digit number after the century separator.
+               if(personNumberString.matches("[0-9]+")){
                Integer personNumber = Integer.parseInt(personNumberString);
                if (personNumber > 0) {
                   // If it was a positive integer, then calculate the checksum.
@@ -120,6 +121,7 @@ public class SSN {
                   }
                }
             }
+         }
          }
       }
       return result;
