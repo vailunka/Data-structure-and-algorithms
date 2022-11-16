ff1803a6db10a7c699815fd6077caf77e46b777a
vailunka19 <vailunka19@student.oulu.fi>
2022-11-16 19:42:50 +0200

    kuvat taulukoista ja lisäykset tiedostoihin

---
+++ 02-mode/src/main/java/oy/tol/tra/Algorithms.java
@@ -0,0 +1,86 @@
+package oy.tol.tra;
+
+public class Algorithms {
+    
+    public static <T extends Comparable<T>> void sort(T [] array) {
+       /*  for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j].compareTo(array[i])<=0) {
+                    swap(array, i, j); 
+                }
+            }
+            }*/
+
+         for(int k = array.length - 1 ; k > -1; k-- ){
+            for(int l = k - 1; l > - 1; l--){
+                if(array[k].compareTo(array[l])<0)
+                swap(array, k, l); 
+            }
+        }
+    }
+   
+
+   public static <T> void reverse(T [] array){
+        int i = 0;
+      while (i < array.length/2) {
+         T temp = array[i];
+         array[i] = array[array.length-i-1];
+         array[array.length-i-1] = temp;
+         i++;
+     }
+        
+    }
+
+    public static <T> void swap(T [] array, int first, int second){
+        if(first != second){
+            T tmp = array[first];
+            array[first] = array[second];
+            array[second] = tmp;
+        }
+    }
+
+    public static class ModeSearchResult<T> {
+        public T theMode;
+        public int count;
+     }
+    public static <T extends Comparable<T>> ModeSearchResult<T> findMode(T [] array) {
+        ModeSearchResult<T> result = new ModeSearchResult<>();
+        int maxcount = 1;
+        int count = 1;
+        T maxvalue = null;
+        int i = 0;
+        
+        if(array == null || array.length <= 1){
+            result.count = -1;
+            result.theMode = null;
+            return result;
+    } 
+       
+        sort(array);
+        maxvalue = array[0];
+        for(i = 0; i < array.length - 1; i++){
+                if(array[i].compareTo(array[i+1]) == 0){
+                    count++;
+                    }
+        
+                else{
+                    if(count >= maxcount){
+                        maxcount = count;
+                        maxvalue = array[i];
+                        count = 1;
+                    }
+                }
+        }
+        if(count >= maxcount){
+            maxcount = count;
+            maxvalue = array[i];
+            
+        }
+
+        
+        result.count = maxcount;
+        result.theMode = maxvalue;
+        return result;
+        
+        }
+}
